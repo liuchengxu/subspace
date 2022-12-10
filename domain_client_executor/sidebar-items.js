4cf8df7b981adba2cac48cfedba5b4a573b8c07d
liuchengxu <xuliuchengxlc@gmail.com>
2022-12-10 09:11:15 +0000

    Deploying to gh-pages from @ liuchengxu/subspace@3a076b13586c909f76604fa81e2fb8ee759443e1 🚀

---
+++ domain_client_executor/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"struct":[["CoreExecutor","Core domain executor."],["CoreGossipMessageValidator","Gossip message validator for core domain."],["EssentialExecutorParams",""],["SystemExecutor","System domain executor."],["SystemGossipMessageValidator","Gossip message validator for system domain."]]};
--- source-files.js
+++ source-files.js
@@ -1,48 +1,65 @@
-var sourcesIndex = {};
-sourcesIndex["cirrus_block_builder"] = {"name":"","files":["lib.rs"]};
-sourcesIndex["cirrus_client_executor"] = {"name":"","files":["aux_schema.rs","bundle_processor.rs","bundle_producer.rs","lib.rs","merkle_tree.rs","worker.rs"]};
-sourcesIndex["cirrus_client_executor_gossip"] = {"name":"","files":["lib.rs","worker.rs"]};
-sourcesIndex["cirrus_node"] = {"name":"","files":["lib.rs","rpc.rs","service.rs"]};
-sourcesIndex["cirrus_pallet_executive"] = {"name":"","files":["lib.rs"]};
-sourcesIndex["cirrus_primitives"] = {"name":"","files":["lib.rs"]};
-sourcesIndex["cirrus_runtime"] = {"name":"","files":["lib.rs","runtime.rs"]};
-sourcesIndex["cirrus_test_runtime"] = {"name":"","files":["lib.rs","runtime.rs"]};
-sourcesIndex["cirrus_test_service"] = {"name":"","files":["chain_spec.rs","lib.rs"]};
-sourcesIndex["cumulus_client_consensus_relay_chain"] = {"name":"","files":["import_queue.rs","lib.rs"]};
-sourcesIndex["orml_vesting"] = {"name":"","files":["lib.rs","weights.rs"]};
-sourcesIndex["pallet_executor"] = {"name":"","files":["lib.rs"]};
-sourcesIndex["pallet_feeds"] = {"name":"","files":["feed_processor.rs","lib.rs"]};
-sourcesIndex["pallet_grandpa_finality_verifier"] = {"name":"","files":["chain.rs","grandpa.rs","lib.rs"]};
-sourcesIndex["pallet_object_store"] = {"name":"","files":["lib.rs"]};
-sourcesIndex["pallet_offences_subspace"] = {"name":"","files":["lib.rs"]};
-sourcesIndex["pallet_rewards"] = {"name":"","files":["default_weights.rs","lib.rs"]};
-sourcesIndex["pallet_runtime_configs"] = {"name":"","files":["lib.rs"]};
-sourcesIndex["pallet_subspace"] = {"name":"","files":["default_weights.rs","equivocation.rs","lib.rs"]};
-sourcesIndex["pallet_transaction_fees"] = {"name":"","files":["default_weights.rs","lib.rs"]};
-sourcesIndex["sc_consensus_fraud_proof"] = {"name":"","files":["lib.rs"]};
-sourcesIndex["sc_consensus_subspace"] = {"name":"","files":["archiver.rs","aux_schema.rs","lib.rs","notification.rs","slot_worker.rs"]};
-sourcesIndex["sc_consensus_subspace_rpc"] = {"name":"","files":["lib.rs"]};
-sourcesIndex["sc_network_test"] = {"name":"","files":["lib.rs"]};
-sourcesIndex["sc_subspace_chain_specs"] = {"name":"","files":["lib.rs","utils.rs"]};
-sourcesIndex["sp_consensus_subspace"] = {"name":"","files":["digests.rs","inherents.rs","lib.rs","offence.rs","verification.rs"]};
-sourcesIndex["sp_executor"] = {"name":"","files":["lib.rs"]};
-sourcesIndex["sp_objects"] = {"name":"","files":["lib.rs"]};
-sourcesIndex["subspace_archiving"] = {"name":"","dirs":[{"name":"archiver","files":["record_shards.rs"]}],"files":["archiver.rs","lib.rs","merkle_tree.rs","reconstructor.rs","utils.rs"]};
-sourcesIndex["subspace_core_primitives"] = {"name":"","files":["crypto.rs","lib.rs","objects.rs"]};
-sourcesIndex["subspace_farmer"] = {"name":"","dirs":[{"name":"commitments","files":["databases.rs","metadata.rs"]}],"files":["archiving.rs","commitments.rs","dsn.rs","farming.rs","identity.rs","lib.rs","multi_farming.rs","node_rpc_client.rs","object_mappings.rs","plot.rs","plotting.rs","rpc_client.rs","single_plot_farm.rs","ws_rpc_server.rs"]};
-sourcesIndex["subspace_fraud_proof"] = {"name":"","files":["lib.rs"]};
-sourcesIndex["subspace_networking"] = {"name":"","dirs":[{"name":"behavior","files":["custom_record_store.rs"]}],"files":["behavior.rs","create.rs","lib.rs","multimess.rs","node.rs","node_runner.rs","pieces_by_range_handler.rs","request_responses.rs","shared.rs","utils.rs"]};
-sourcesIndex["subspace_node"] = {"name":"","dirs":[{"name":"secondary_chain","files":["chain_spec.rs","cli.rs"]}],"files":["chain_spec.rs","chain_spec_utils.rs","import_blocks_from_dsn.rs","lib.rs","secondary_chain.rs"]};
-sourcesIndex["subspace_rpc_primitives"] = {"name":"","files":["lib.rs"]};
-sourcesIndex["subspace_runtime"] = {"name":"","files":["feed_processor.rs","fees.rs","lib.rs","object_mapping.rs","signed_extensions.rs"]};
-sourcesIndex["subspace_runtime_primitives"] = {"name":"","files":["lib.rs"]};
-sourcesIndex["subspace_service"] = {"name":"","files":["lib.rs","pool.rs","rpc.rs"]};
-sourcesIndex["subspace_solving"] = {"name":"","files":["codec.rs","lib.rs"]};
-sourcesIndex["subspace_test_client"] = {"name":"","files":["chain_spec.rs","lib.rs"]};
-sourcesIndex["subspace_test_runtime"] = {"name":"","files":["lib.rs"]};
-sourcesIndex["subspace_test_service"] = {"name":"","files":["lib.rs"]};
-sourcesIndex["subspace_wasm_tools"] = {"name":"","files":["lib.rs"]};
-sourcesIndex["substrate_test_runtime"] = {"name":"","files":["genesismap.rs","lib.rs","system.rs"]};
-sourcesIndex["substrate_test_runtime_client"] = {"name":"","files":["block_builder_ext.rs","lib.rs","trait_tests.rs"]};
-sourcesIndex["substrate_test_runtime_transaction_pool"] = {"name":"","files":["lib.rs"]};
+var sourcesIndex = JSON.parse('{\
+"core_payments_domain_runtime":["",[],["lib.rs","runtime.rs"]],\
+"cross_domain_message_gossip":["",[],["gossip_worker.rs","lib.rs","message_listener.rs"]],\
+"domain_block_builder":["",[],["lib.rs"]],\
+"domain_client_consensus_relay_chain":["",[],["import_queue.rs","lib.rs"]],\
+"domain_client_executor":["",[],["aux_schema.rs","bundle_election_solver.rs","core_bundle_processor.rs","core_bundle_producer.rs","core_domain_worker.rs","core_executor.rs","core_gossip_message_validator.rs","domain_block_processor.rs","domain_bundle_producer.rs","domain_bundle_proposer.rs","domain_worker.rs","fraud_proof.rs","gossip_message_validator.rs","lib.rs","merkle_tree.rs","system_bundle_processor.rs","system_bundle_producer.rs","system_domain_worker.rs","system_executor.rs","system_gossip_message_validator.rs","utils.rs"]],\
+"domain_client_executor_gossip":["",[],["lib.rs","worker.rs"]],\
+"domain_client_message_relayer":["",[],["lib.rs","worker.rs"]],\
+"domain_pallet_executive":["",[],["lib.rs"]],\
+"domain_runtime_primitives":["",[],["lib.rs"]],\
+"domain_service":["",[],["core_domain.rs","lib.rs","rpc.rs","system_domain.rs"]],\
+"domain_test_runtime":["",[],["lib.rs","runtime.rs"]],\
+"domain_test_service":["",[],["chain_spec.rs","lib.rs"]],\
+"orml_vesting":["",[],["lib.rs","weights.rs"]],\
+"pallet_domain_registry":["",[],["lib.rs"]],\
+"pallet_domain_tracker":["",[],["lib.rs"]],\
+"pallet_domains":["",[],["lib.rs"]],\
+"pallet_executor_registry":["",[],["lib.rs"]],\
+"pallet_feeds":["",[],["feed_processor.rs","lib.rs"]],\
+"pallet_grandpa_finality_verifier":["",[],["chain.rs","grandpa.rs","lib.rs"]],\
+"pallet_messenger":["",[],["fees.rs","lib.rs","messages.rs","relayer.rs","verification.rs"]],\
+"pallet_object_store":["",[],["lib.rs"]],\
+"pallet_offences_subspace":["",[],["lib.rs"]],\
+"pallet_rewards":["",[],["default_weights.rs","lib.rs"]],\
+"pallet_runtime_configs":["",[],["lib.rs"]],\
+"pallet_subspace":["",[],["default_weights.rs","equivocation.rs","lib.rs"]],\
+"pallet_transaction_fees":["",[],["default_weights.rs","lib.rs"]],\
+"pallet_transporter":["",[],["lib.rs"]],\
+"sc_consensus_fraud_proof":["",[],["lib.rs"]],\
+"sc_consensus_subspace":["",[],["archiver.rs","aux_schema.rs","lib.rs","notification.rs","slot_worker.rs"]],\
+"sc_consensus_subspace_rpc":["",[],["lib.rs"]],\
+"sc_network_test":["",[],["lib.rs"]],\
+"sc_subspace_chain_specs":["",[],["lib.rs","utils.rs"]],\
+"sp_consensus_subspace":["",[],["digests.rs","inherents.rs","lib.rs","offence.rs"]],\
+"sp_domain_digests":["",[],["lib.rs"]],\
+"sp_domain_tracker":["",[],["lib.rs"]],\
+"sp_domains":["",[],["bundle_election.rs","fraud_proof.rs","lib.rs","transaction.rs"]],\
+"sp_executor_registry":["",[],["lib.rs"]],\
+"sp_lightclient":["",[],["lib.rs"]],\
+"sp_messenger":["",[],["endpoint.rs","lib.rs","messages.rs"]],\
+"sp_objects":["",[],["lib.rs"]],\
+"subspace_archiving":["",[["archiver",[],["record_shards.rs"]]],["archiver.rs","lib.rs","reconstructor.rs","utils.rs"]],\
+"subspace_core_primitives":["",[["crypto",[["kzg",[],["serde.rs"]]],["kzg.rs"]]],["crypto.rs","lib.rs","objects.rs","sector_codec.rs"]],\
+"subspace_farmer":["",[["rpc_client",[],["node_rpc_client.rs"]],["single_disk_plot",[],["piece_publisher.rs","piece_reader.rs","piece_receiver.rs"]]],["identity.rs","lib.rs","object_mappings.rs","reward_signing.rs","rpc_client.rs","single_disk_plot.rs","utils.rs","ws_rpc_server.rs"]],\
+"subspace_farmer_components":["",[],["farming.rs","file_ext.rs","lib.rs","plotting.rs"]],\
+"subspace_fraud_proof":["",[],["lib.rs"]],\
+"subspace_networking":["",[["behavior",[],["custom_record_store.rs","persistent_parameters.rs","record_binary_heap.rs"]],["request_handlers",[],["generic_request_handler.rs","object_mappings.rs","peer_info.rs","piece_by_key.rs","pieces_by_range.rs"]],["utils",[],["multihash.rs","prometheus.rs"]]],["behavior.rs","create.rs","lib.rs","node.rs","node_runner.rs","request_handlers.rs","request_responses.rs","shared.rs","utils.rs"]],\
+"subspace_node":["",[["core_domain",[],["cli.rs","core_payments_chain_spec.rs"]],["secondary_chain",[],["chain_spec.rs","cli.rs"]]],["chain_spec.rs","chain_spec_utils.rs","core_domain.rs","import_blocks_from_dsn.rs","lib.rs","secondary_chain.rs"]],\
+"subspace_rpc_primitives":["",[],["lib.rs"]],\
+"subspace_runtime":["",[["weights",[],["subspace.rs"]]],["domains.rs","feed_processor.rs","fees.rs","lib.rs","object_mapping.rs","signed_extensions.rs","weights.rs"]],\
+"subspace_runtime_primitives":["",[],["lib.rs"]],\
+"subspace_service":["",[],["dsn.rs","lib.rs","piece_cache.rs","pool.rs","rpc.rs"]],\
+"subspace_solving":["",[],["lib.rs"]],\
+"subspace_test_client":["",[],["chain_spec.rs","lib.rs"]],\
+"subspace_test_runtime":["",[],["lib.rs"]],\
+"subspace_test_service":["",[],["lib.rs"]],\
+"subspace_verification":["",[],["lib.rs"]],\
+"subspace_wasm_tools":["",[],["lib.rs"]],\
+"substrate_test_runtime":["",[],["genesismap.rs","lib.rs","system.rs"]],\
+"substrate_test_runtime_client":["",[],["block_builder_ext.rs","lib.rs","trait_tests.rs"]],\
+"substrate_test_runtime_transaction_pool":["",[],["lib.rs"]],\
+"system_domain_runtime":["",[],["lib.rs","runtime.rs"]],\
+"system_runtime_primitives":["",[],["lib.rs"]]\
+}');
 createSourceSidebar();
